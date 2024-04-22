cda5cd697566b6d97e28bc736e01c077f332e507
James <jamesclarkedublin@gmail.com>
2024-04-22 23:47:27 +0100

    Code cleaned up

--- CameraBoid.gd
+++ CameraBoid.gd
@@ -5,6 +5,10 @@ var boid
 func _ready():
 	boid = get_tree().current_scene.find_child("Boid")
 
-func _physics_process(delta):	
-	var desired = global_transform.looking_at(boid.global_transform.origin, Vector3.UP)		
-	self.global_transform.basis = global_transform.basis.slerp(desired.basis, delta * 2).orthonormalized()
+func lookAtBoid(delta):
+	var desired = global_transform.looking_at(boid.global_transform.origin, Vector3.UP)  #Get the desired orientation
+	self.global_transform.basis = global_transform.basis.slerp(desired.basis, delta * 2).orthonormalized() #slerp to orientation
+
+func _physics_process(delta):
+	lookAtBoid(delta)
+
